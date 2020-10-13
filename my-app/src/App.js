57003171a44330ab5240b74af0ec12744bef9653
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-10-14 08:41:28 +0900

    study react fragment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello, world!</h1>
+    // const greeting = "Hello, React!";
+    return (
+        // return以下は一つのタグを返すようにする、これは仕様。二つ以上返す場合は空タグで囲えばOK */}
+      <>
+        <label htmlFor="bar">bar</label>
+        < input input type="text" onClick={() => { console.log("clicked!") }}></input>
+      </ >
+    )
   }
 }
 
